588f536a5aa8b365b2cf6169b2fa928a7044cf9e
jp <jonahpatinkin@gmail.com>
2025-06-08 00:07:39 -0400

    added some test code

--- hellopy.py
+++ hellopy.py
@@ -1,3 +1,8 @@
 import subprocess
 
-print("Asdfasdsdfsfdasdf")
+def insecure_function():
+    user_input = "ls -la"
+    subprocess.call(user_input, shell=True)
+
+if __name__ == "__main__":
+    insecure_function()
