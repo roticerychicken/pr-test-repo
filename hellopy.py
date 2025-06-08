6431805001b39350deeb540c76076db34a362a12
jp <jonahpatinkin@gmail.com>
2025-06-08 14:27:16 -0400

    added a print statement

--- hellopy.py
+++ hellopy.py
@@ -1,8 +1,3 @@
 import subprocess
 
-def insecure_function():
-    user_input = "ls -la"
-    subprocess.call(user_input, shell=True)
-
-if __name__ == "__main__":
-    insecure_function()
+print("asdfasdfsad")
