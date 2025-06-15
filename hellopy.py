ac16162406fb5aec8bc04880320aad989b0026b9
jp <jonahpatinkin@gmail.com>
2025-06-15 15:11:21 -0400

    testing upload

--- hellopy.py
+++ hellopy.py
@@ -4,7 +4,3 @@ import pickle
 
 # Dangeroasdfsus use of subprocess
 subprocess.call("ls -la", shell=True)
-
-# Dangerous use of pickle
-malicious_data = b"cos\nsystem\n(S'echo hacked'\ntR."
-pickle.loads(malicious_data)
